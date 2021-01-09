477ef0395dc3b3c51c148802bb13e20eb5aae2ea
solrak <luiscarlosquesada@gmail.com>
2021-01-09 15:01:26 -0600

    Enviroment  Config ready to deploy

---
+++ config.py
@@ -0,0 +1,17 @@
+from decouple import config
+
+class Coinfig:
+    pass
+
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig
+}
